ed54b22cefce5008445890feb88553360602d2c4
ibtesam5d <ibtesam.hossain@gmail.com>
2023-02-22 11:41:23 -0700

    changes to model

---
+++ models/Product.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: 60,
+    },
+    desc: {
+      type: String,
+      required: true,
+      maxlength: 200,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    prices: {
+      type: [Number],
+      required: true,
+    },
+    materialType: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Product ||
+  mongoose.model("Product", productSchema);
